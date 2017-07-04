1cb5dfbabb4bcc05368b8cf93c0fba792f307f03
Jed <johon8@gmail.com>
2017-07-04 13:50:47 -0400

    added dev dep

--- app/index.js
+++ app/index.js
@@ -131,6 +131,8 @@ module.exports = generator.extend({
             packageFile.devDependencies["babel-preset-react"] = "latest";
             packageFile.devDependencies["gulp"] = "latest";
             packageFile.devDependencies["gulp-util"] = "latest";
+            packageFile.devDependencies["eslint"] = "latest";
+            packageFile.devDependencies["eslint-plugin-react"] = "latest";
 
             this.fs.writeJSON(
                 this.destinationPath('package.json'),
